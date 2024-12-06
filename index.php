858a951a7ad9ed8e544b58304839ed4a579ce154
kunalgp19 <kunalgp2019@gmail.com>
2024-12-06 12:07:35 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<h1>This is sellmyprod Ecommerce Webpage</h1>
+<h1>Home Page</h1>
+<?php echo "Click to buy Products"; ?>
+<?php echo "Some enhancement is done on this website"; ?>
+</body>
+</html>
